7e533cf9359db51201feff77e45103dcc271ea03
Hasanli Hacaga <hasanlihm@KapMac437-M.local>
2023-08-02 00:21:11 +0400

    Webpack Config for dev and common

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -1,7 +1,20 @@
 module.exports = {
-    module:{
-        rules:[
-            
-        ]
-    }
-}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/, // extension .js ve ya mjs ile bitirse babel terefinden test edilmesini isteyirik
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            // preset-react: babel will process all the jsx tags that is added to our file (related-code)
+            // preset-env: convert all the ES6 ES7 ES8 to ES5
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            //  
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
