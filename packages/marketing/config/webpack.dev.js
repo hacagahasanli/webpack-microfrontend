0746fad83ad8cd273e4bb5b7e69eedab8afd7cb5
HasanliHM <hacaga.hasanli@kapitalbank.az>
2023-08-06 00:16:44 +0400

    ....

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,16 +1,24 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
   mode: 'development',
   devServer: {
-    port: 8081,
+    port: 7081,
     historyApiFallback: {
       index: 'index.html',
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Marketing': './src/bootstrap',
+      },
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
