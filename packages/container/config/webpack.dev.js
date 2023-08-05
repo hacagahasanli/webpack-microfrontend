7bda2833221167fae711b69d53258902fd4f5b3b
HasanliHM <hacaga.hasanli@kapitalbank.az>
2023-08-06 00:22:17 +0400

    ...

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 7080,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
